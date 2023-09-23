20fb25c1256f2ad076d2112b6b6ebc73017ea5e2
ido barnea <barbareshet@gmail.com>
2023-09-23 17:01:21 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const API = {
     key : process.env.REACT_APP_PUBLIC_WEATHER_API_KEY,
-    base_api_url : process.env.REACT_APP_PUBLIC_WEATHER_API_KEY_BASE_URL || "https://api.openweathermap.org/data/2.5/",
+    base_api_url : process.env.REACT_APP_PUBLIC_WEATHER_API_BASE_URL || "https://api.openweathermap.org/data/2.5/",
     googleMapsBase: process.env.REACT_APP_PUBLIC_GOOGLE_MAPS_BASE_URL || "https://maps.googleapis.com/maps/api/geocode/",
     googleMapsKey: process.env.REACT_APP_PUBLIC_GOOGLE_MAPS_KEY,
 }
