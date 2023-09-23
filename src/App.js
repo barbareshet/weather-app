bae8534b6d155faf878a265e95ad01914f4c4819
ido barnea <barbareshet@gmail.com>
2023-09-23 09:35:23 +0300

    config file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-
+import API from "./config";
 
 function App() {
+
+  console.log(API.key, API.base_api_url)
   return (
     <div className="App">
 
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const API = {
+    key : process.env.WEATHER_API_KEY,
+    base_api_url : process.env.WEATHER_API_KEY_BASE_URL
+}
+export default API
