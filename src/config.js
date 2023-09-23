95d1db46d4a4b691bbe47386b35c11ee90e9009c
ido barnea <barbareshet@gmail.com>
2023-09-23 16:30:38 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const API = {
-    key : process.env.WEATHER_API_KEY,
-    base_api_url : process.env.WEATHER_API_KEY_BASE_URL || "https://api.openweathermap.org/data/2.5/",
-    googleMapsBase: process.env.GOOGLE_MAPS_BASE_URL || "https://maps.googleapis.com/maps/api/geocode/",
-    googleMapsKey: process.env.GOOGLE_MAPS_KEY,
+    key : process.env.NEXT_WEATHER_API_KEY,
+    base_api_url : process.env.NEXT_PUBLIC_WEATHER_API_KEY_BASE_URL || "https://api.openweathermap.org/data/2.5/",
+    googleMapsBase: process.env.NEXT_PUBLIC_GOOGLE_MAPS_BASE_URL || "https://maps.googleapis.com/maps/api/geocode/",
+    googleMapsKey: process.env.NEXT_GOOGLE_MAPS_KEY,
 }
 export default API
